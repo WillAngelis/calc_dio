692ca81826de53fd6e6e50c1629ee3ea5b03a614
Will Angelis <willangelis@gmail.com>
2022-12-24 03:41:37 -0300

    test: remove testes do componente App
    Os testes do componente App foram removidos devido a mudanças significativas na estrutura da aplicação. Eles serão refeitos e adicionados novamente em uma atualização futura.

--- src/templates/App/App.spec.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from '@testing-library/react';
-import { App } from './App';
-
-it('should test', () => {
-  const { getByText } = render(<App />);
-
-  expect(getByText('Hello World')).toBeInTheDocument();
-});
-
