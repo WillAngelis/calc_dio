8c3795b0a43652982be65e14efa3eedbebe4c08f
Will Angelis <willangelis@gmail.com>
2022-12-26 23:08:50 -0300

    feat: adiciona componente Screen ao projeto

---
+++ src/components/Screen/index.tsx
@@ -0,0 +1,12 @@
+export const Screen = () => {
+    return (
+        <section className="flex text-xl justify-center items-center gap-4 w-80 bg-[#151515] p-4 h-52 flex-col">
+            <div className="flex justify-center w-full text-gray-400 items-center text-sm font-semibold gap-2">
+                <p>3.50</p>
+                <span className="text-primary">*</span>
+                <p>100.25</p>
+            </div>
+            <h2 className="text-white text-4xl font-semibold mb-auto">459.5</h2>
+        </section>
+    );
+};
