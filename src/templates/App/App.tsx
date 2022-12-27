43fb005f3f29085e4b8e17ef62d8836d1472e513
Will Angelis <willangelis@gmail.com>
2022-12-26 23:10:47 -0300

    feat: inclue componentes Screen e ButtonScreen a tela inicial do projeto

--- src/templates/App/App.tsx
+++ src/templates/App/App.tsx
@@ -1,11 +1,14 @@
-import { Container } from "../../components/Container";
-import { Header } from "../../components/Header";
+import { ButtonsScreen } from '../../components/ButtonsScreen';
+import { Container } from '../../components/Container';
+import { Header } from '../../components/Header';
+import { Screen } from '../../components/Screen';
 
 export const App = () => {
-  return (
-   <Container>
-    <Header/>
-   </Container>
-  );
+    return (
+        <Container>
+            <Header />
+            <Screen />
+            <ButtonsScreen />
+        </Container>
+    );
 };
-
