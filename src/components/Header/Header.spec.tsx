df4ffe751ded4356afeb34864995e24a7a44ffff
Will Angelis <willangelis@gmail.com>
2022-12-26 23:10:18 -0300

    test: adiciona mais casos de teste ao componente Header

--- src/components/Header/Header.spec.tsx
+++ src/components/Header/Header.spec.tsx
@@ -1,45 +1,43 @@
 import { screen, render } from '@testing-library/react';
-import { Header } from ".";
+import { Header } from '.';
 
 describe('Header Component', () => {
+    it('should render Header title "Calc" ', () => {
+        render(<Header />);
+        const h1 = screen.getByRole('heading');
 
-  it('should render Header title "Calc" ', () => {
-    render(<Header/>);
-    const h1 = screen.getByRole('heading')
+        expect(h1).toBeInTheDocument();
+    });
 
-    expect(h1).toBeInTheDocument();
-  });
+    it('should render Header title "."', () => {
+        render(<Header />);
 
-  it('should render Header title "."', () => {
-    render(<Header />)
+        const span = screen.getByText('.');
 
-    const span = screen.getByText('.')
+        expect(span).toBeInTheDocument();
+    });
 
-    expect(span).toBeInTheDocument();
+    it('header title has the correct text color and font weight', () => {
+        render(<Header />);
 
-  })
+        const h1 = screen.getByRole('heading');
+        const span = screen.getByText('.');
 
-  it('header title has the correct text color and font weight', () => {
-    render(<Header />);
+        expect(h1).toHaveClass('font-bold');
+        expect(span).toHaveClass('text-primary font-bold');
+    });
 
-    const h1 = screen.getByRole('heading')
-    const span = screen.getByText('.');
+    it('renders the close icon', () => {
+        render(<Header />);
 
-    expect(h1).toHaveClass("font-bold");
-    expect(span).toHaveClass("text-red-400 font-bold");
-  })
+        const closeIcon = screen.getByTitle('close icon');
+        expect(closeIcon).toBeInTheDocument();
+    });
 
-  it('renders the close icon', () => {
-    render(<Header />);
+    it('renders the menu icon', () => {
+        render(<Header />);
 
-    const closeIcon = screen.getByTitle( "close icon")
-    expect(closeIcon).toBeInTheDocument();
-  });
-
-  it('renders the menu icon', () => {
-    render(<Header />);
-
-    const menuIcon = screen.getByTitle( "menu icon")
-    expect(menuIcon).toBeInTheDocument();
-  });
+        const menuIcon = screen.getByTitle('menu icon');
+        expect(menuIcon).toBeInTheDocument();
+    });
 });
