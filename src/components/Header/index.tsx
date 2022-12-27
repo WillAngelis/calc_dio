47d200579bfed52dbda8f0f036dcafc9167ca966
Will Angelis <willangelis@gmail.com>
2022-12-26 23:09:51 -0300

    refactor: aplica regras de formatação do prettier e altera style do componente

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,11 +2,23 @@ import { BiMenuAltLeft } from 'react-icons/bi';
 import { RiCloseLine } from 'react-icons/ri';
 
 export const Header = () => {
-  return (
-    <header className="flex text-xl justify-between w-96">
-      <BiMenuAltLeft title="menu icon" size={32}/>
-      <h1 className="font-bold">Calc<span className="text-red-400 font-bold">.</span></h1>
-      <RiCloseLine  title="close icon" size={32}/>
-    </header>
-  )
-}
+    return (
+        <header className="flex text-base justify-between w-80 bg-[#151515] p-4 py-6 rounded-t-3xl">
+            <BiMenuAltLeft
+                color="white"
+                title="menu icon"
+                size={24}
+                cursor="pointer"
+            />
+            <h1 className="font-bold text-white">
+                Calc<span className="text-primary font-bold">.</span>
+            </h1>
+            <RiCloseLine
+                color="white"
+                title="close icon"
+                size={24}
+                cursor="pointer"
+            />
+        </header>
+    );
+};
