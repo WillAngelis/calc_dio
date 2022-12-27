dbf2d09beea6f66d0dc9426c5f274566421542e3
Will Angelis <willangelis@gmail.com>
2022-12-26 23:07:16 -0300

    style: aplica padrão de formatação do prettier ao código

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,37 +1,36 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    jest: true,
-    node: true,
-  },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react-hooks/recommended',
-    'plugin:prettier/recommended',
-    'plugin:@typescript-eslint/recommended',
-  ],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-  },
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
+    env: {
+        browser: true,
+        es2021: true,
+        jest: true,
+        node: true,
     },
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-  },
-  plugins: ['react', 'prettier', 'react-hooks', '@typescript-eslint'],
-  settings: {
-    react: {
-      version: 'detect',
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:react-hooks/recommended',
+        'plugin:prettier/recommended',
+        'plugin:@typescript-eslint/recommended',
+    ],
+    globals: {
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly',
     },
-  },
-  rules: {
-    'react/react-in-jsx-scope': 'off',
-  },
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    plugins: ['react', 'prettier', 'react-hooks', '@typescript-eslint'],
+    settings: {
+        react: {
+            version: 'detect',
+        },
+    },
+    rules: {
+        'react/react-in-jsx-scope': 'off',
+    },
 };
-
--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,19 +1,6 @@
 module.exports = {
-  arrowParens: 'always',
-  bracketSpacing: true,
-  endOfLine: 'crlf',
-  htmlWhitespaceSensitivity: 'ignore',
-  insertPragma: true,
-  jsxSingleQuote: false,
-  printWidth: 80,
-  proseWrap: 'always',
-  quoteProps: 'as-needed',
-  requirePragma: true,
-  semi: true,
-  singleQuote: true,
-  tabWidth: 2,
-  trailingComma: 'all',
-  useTabs: true,
-  embeddedLanguageFormatting: 'off',
-};
-
+    trailingComma: 'es5',
+    tabWidth: 4,
+    semi: true,
+    singleQuote: true,
+}
