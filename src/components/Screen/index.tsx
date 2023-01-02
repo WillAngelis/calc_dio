efe8ef7b25c3547ae5f6048ce793b4149bfaf324
Will Angelis <willangelis@gmail.com>
2023-01-02 01:48:17 -0300

    feat: add result, operator and numbers in screen

--- src/components/Screen/index.tsx
+++ src/components/Screen/index.tsx
@@ -1,12 +1,20 @@
+import { Calculator } from '../../context/Calculator';
+import { useContext } from 'react';
+
 export const Screen = () => {
+    const { result, operator, firstValue, secondValue } =
+        useContext(Calculator);
+
     return (
         <section className="flex text-xl justify-center items-center gap-4 w-80 bg-[#151515] p-4 h-52 flex-col">
-            <div className="flex justify-center w-full text-gray-400 items-center text-sm font-semibold gap-2">
-                <p>3.50</p>
-                <span className="text-primary">*</span>
-                <p>100.25</p>
+            <div className="flex justify-center w-full text-gray-400 items-center text-sm font-semibold gap-2 h-4">
+                <p>{firstValue}</p>
+                <span className="text-primary">{operator}</span>
+                <p>{secondValue}</p>
             </div>
-            <h2 className="text-white text-4xl font-semibold mb-auto">459.5</h2>
+            <h2 className="text-white text-4xl font-semibold mb-auto">
+                {result}
+            </h2>
         </section>
     );
 };
