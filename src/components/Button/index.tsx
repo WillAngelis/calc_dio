21e072e6a9da7eeed892e41f3d22252fd96ba64b
Will Angelis <willangelis@gmail.com>
2023-01-02 01:49:23 -0300

    feat: now calculator works with 4 basic operations

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,3 +1,6 @@
+import { useContext } from 'react';
+import { Calculator } from '../../context/Calculator';
+
 export enum ButtonType {
     Numeric = 'NUMERIC',
     Operator = 'OPERATOR',
@@ -7,8 +10,8 @@ export enum ButtonType {
 
 interface ButtonsProps {
     type: ButtonType;
-    name: string | number;
-    onClick: () => void;
+    name: string;
+    onClick: (type: ButtonType) => void;
 }
 
 const getButtonStyle = (type: ButtonType) => {
@@ -23,9 +26,90 @@ const getButtonStyle = (type: ButtonType) => {
             return 'text-white h-14 w-14 hover:bg-[#303030] hover:rounded-full hover:border-4 hover:border-[#212121]';
     }
 };
-export const Button = ({ name, type, onClick }: ButtonsProps) => {
+export const Button = ({ name, type }: ButtonsProps) => {
+    const {
+        setResult,
+        setOperator,
+        setFirstValue,
+        setSecondValue,
+        firstValue,
+        secondValue,
+        isSecondNumber,
+        operator,
+        setIsSecondNumber,
+    } = useContext(Calculator);
+
+    const handleChangeValueInNumbers = () => {
+        if (firstValue === undefined) {
+            setFirstValue(name);
+        } else if (!isSecondNumber) {
+            setFirstValue((previousValue) => `${previousValue}${name}`);
+        } else if (isSecondNumber && secondValue === undefined) {
+            setSecondValue(name);
+        } else {
+            setSecondValue((previousValue) => `${previousValue}${name}`);
+        }
+    };
+
+    const handleResult = () => {
+        switch (operator) {
+            case '+':
+                setResult(
+                    parseInt(firstValue as string) +
+                        parseInt(secondValue as string)
+                );
+                break;
+
+            case '-':
+                setResult(
+                    parseInt(firstValue as string) -
+                        parseInt(secondValue as string)
+                );
+                break;
+            case '*':
+                setResult(
+                    parseInt(firstValue as string) *
+                        parseInt(secondValue as string)
+                );
+
+                break;
+            case '/':
+                setResult(
+                    parseInt(firstValue as string) /
+                        parseInt(secondValue as string)
+                );
+
+                break;
+            default:
+                break;
+        }
+    };
+
+    const handleChange = () => {
+        switch (type) {
+            case ButtonType.Clear:
+                setResult(0);
+                setFirstValue(undefined);
+                setSecondValue(undefined);
+                setOperator(undefined);
+                setIsSecondNumber(false);
+                break;
+            case ButtonType.Operator:
+                if (name === '=') return handleResult();
+                setIsSecondNumber(true);
+                setOperator(name);
+                break;
+            case ButtonType.Numeric:
+                handleChangeValueInNumbers();
+                break;
+
+            default:
+                break;
+        }
+    };
+
     return (
-        <button className={getButtonStyle(type)} onClick={onClick}>
+        <button className={getButtonStyle(type)} onClick={handleChange}>
             {name}
         </button>
     );
